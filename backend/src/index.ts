593d760d9d7c74255b015b49c6102a94c427dbe3
dveo26 <devansh26.01.2005@gmail.com>
2025-06-27 23:48:45 +0530

    modified:   src/index.ts error logging

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,6 +12,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 connectDB();
+console.log('connected to mongoose');
 app.use('/api/auth', authRoutes);
 app.use('/api/content', contentRoutes);
 app.use('/api/share', shareRoutes);
